698810ee1b4f20f0855aac7be7acb72ba3cccbbd
belifkutlu <belifkutlu@gmail.com>
2022-09-19 12:57:49 +0300

    add StarshipDetail page

---
+++ src/pages/StarshipDetail/StarshipDetail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StarshipTitle, Name } from "./StarshipDetail.styled";
+
+function StarshipDetail() {
+  return <StarshipTitle>StarshipDetail Page</StarshipTitle>;
+}
+
+export default StarshipDetail;
---
+++ src/pages/StarshipDetail/StarshipDetail.styled.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+
+export const StarshipTitle = styled.div``;
+export const Name = styled.div``;
