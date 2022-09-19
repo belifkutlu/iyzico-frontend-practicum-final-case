564566c420c4de2e72b1d2dc6858a577aa340f5e
belifkutlu <belifkutlu@gmail.com>
2022-09-19 14:55:39 +0300

    add load more

--- src/pages/Starships/Starships.jsx
+++ src/pages/Starships/Starships.jsx
@@ -1,59 +1,48 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styled from "styled-components";
 
-import "./starships.css";
+import { StarshipList, StarshipCard, StarshipName } from "./Starships.styled";
 
 function Starships() {
   const [starhips, setStarships] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
   console.log(starhips);
 
   useEffect(() => {
     axios
-      .get("https://swapi.dev/api/starships/?page=1")
+      .get(`https://swapi.dev/api/starships/?page=${page}`)
       .then((response) => {
         const data = response.data.results;
-        setStarships(data);
+        setStarships((prevData) => [...prevData, ...data]);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [page]);
 
   return (
     <div>
       <h1>Starships Page</h1>
-      <StarshipList>
-        {starhips.map((starship) => {
-          return (
-            <StarshipCard key={starship.name}>
-              <StarshipName> {starship.name}</StarshipName>
-              <p>{starship.model}</p>
-            </StarshipCard>
-          );
-        })}
-      </StarshipList>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <StarshipList>
+          {starhips.map((starship) => {
+            return (
+              <StarshipCard key={starship.name}>
+                <StarshipName> {starship.name}</StarshipName>
+                <p>{starship.model}</p>
+              </StarshipCard>
+            );
+          })}
+          <button onClick={() => setPage(page + 1)}>load more...</button>
+        </StarshipList>
+      )}
     </div>
   );
 }
-
-const StarshipList = styled.div`
-  background-color: #b07373;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
-const StarshipCard = styled.div`
-  border: 1px solid saddlebrown;
-  background-color: white;
-  padding: 10px;
-  margin: 1%;
-  width: 23%;
-`;
-
-const StarshipName = styled.h3`
-  color: red;
-`;
 
 export default Starships;
---
+++ src/pages/Starships/Starships.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StarshipList = styled.div`
+  background-color: #b07373;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const StarshipCard = styled.div`
+  border: 1px solid saddlebrown;
+  background-color: white;
+  padding: 10px;
+  margin: 1%;
+  width: 23%;
+  border-radius: 5px;
+`;
+
+export const StarshipName = styled.h3`
+  color: red;
+`;
