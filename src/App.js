8a7ec96d8ad55d001691c99548d35fe4c67b4820
belifkutlu <belifkutlu@gmail.com>
2022-09-26 13:07:07 +0300

    add Introduction page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { Route, Routes } from "react-router-dom";
 
 import Starships from "./pages/Starships";
 import StarshipDetail from "./pages/StarshipDetail";
+import Introduction from "./pages/Introduction";
+import { StarshipProvider } from "./contexts/starshipContext";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Starships />} />
-        <Route path="/starship-detail" element={<StarshipDetail />} />
-      </Routes>
+      <StarshipProvider>
+        <Routes>
+          <Route path="/" element={<Starships />} />
+          <Route path="/intro" element={<Introduction />} />
+          <Route path="/starship-detail" element={<StarshipDetail />} />
+        </Routes>
+      </StarshipProvider>
     </div>
   );
 }
